02f91e969a7fdcfb5565d11cd7433b0a717ae34c
knileshh <neutron_stays@protonmail.com>
2025-12-10 00:08:26 +0530

    Initial setup: Monorepo structure with 4 services, Parent POM, and Docker infrastructure

---
+++ order-service/src/main/java/com/cravecourier/order/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cravecourier.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
